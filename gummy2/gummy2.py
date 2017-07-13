d08d5739bec73ba114eee230b0c5f381ded7e711
John Neil Soo <neil.soo@adish.co.jp>
2017-07-13 14:21:09 +0800

    made translating to ajax

--- gummy2/gummy2.py
+++ gummy2/gummy2.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, g, redirect, url_for, abort, render_template, flash
+from flask import Flask, request, g, redirect, url_for, abort, render_template, flash, jsonify
 from flask_bootstrap import Bootstrap
 from translate import Translator
 import urllib
@@ -13,17 +13,17 @@ app.config.from_object(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/ja_translate', methods=['POST'])
+@app.route('/_ja_translate', methods=['GET'])
 def ja_translate():
-    text = request.form['text_translate'].encode('utf-8')
+    text = request.args.get('text', '', type=str)
     translate = Translator(to_lang='ja', from_lang='en-US')
     translated_text = translate.translate(text)
-    return render_template('home.html', translated_ja_text=translated_text, translated_en_text=text)
+    return jsonify(translated_text)
 
-@app.route('/en_translate', methods=['POST'])
+@app.route('/_en_translate', methods=['GET'])
 def en_translate():
-    text = request.form['text_translate']
+    text = request.args.get('text', '', type=unicode)
     translate = Translator(to_lang='en-US', from_lang='ja')
     translated_text = urllib.quote(translate.translate(text.encode('utf-8')))
-    return render_template('home.html', translated_en_text=translated_text, translated_ja_text=text)
+    return jsonify(translated_text)
 
